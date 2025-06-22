c1c9bacae60b7592021626aa1724bb22569af1e5
CodeLine6 <chaudharymaneet93@gmail.com>
2025-06-22 17:22:40 +0530

    feat: Initialize project with core structure and assets
    This commit initializes the project with the basic file structure, core components, assets (images, videos, fonts, audio), configuration files (Vite, ESLint), and initial React components. This sets up the foundation for building the application.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Features from "./components/Features"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Story from "./components/Story"
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <Features/>
+      <Story />
+      <Contact />
+    </main>
+  )
+}
+
+export default App
